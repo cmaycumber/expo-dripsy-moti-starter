64dbf40628d8a836f13d25a77035059f00c5ad11
Chadwick Maycumber <chad.maycumber11@gmail.com>
2021-03-04 21:09:06 -0500

    initial setup

--- App.tsx
+++ App.tsx
@@ -1,21 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, DripsyProvider, View } from "dripsy";
+import { StatusBar } from "expo-status-bar";
+import { View as MotiView } from "moti";
+import React from "react";
+import { StyleSheet } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <DripsyProvider
+      theme={{
+        colors: {
+          primary: "green",
+        },
+      }}
+    >
+      <View
+        sx={{
+          flex: 1,
+          backgroundColor: "#fff",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <MotiView
+          animate={{ opacity: 1, translateY: 0 }}
+          from={{ opacity: 0, translateY: -100 }}
+        >
+          <Text>Open up App.tsx to start working on your app!</Text>
+        </MotiView>
+        <StatusBar style="auto" />
+      </View>
+    </DripsyProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
